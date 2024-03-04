14265d383ad7f93911d5457aa998f83c3413087e
vivekmaru36 <vivekmaru036@gmail.com>
2024-03-04 16:27:23 +0530

    4..

--- RFid/src/Components/Queries/AttendanceStudent.js
+++ RFid/src/Components/Queries/AttendanceStudent.js
@@ -12,6 +12,27 @@ const AttendanceStudent = () => {
   // const [date, setDate] = useState("");
   const [error, setError] = useState("");
 
+  const [AllLecs,setLecs]=useState([]);
+
+  const fetchallLectures=async(e)=>{
+    e.preventDefault();
+    setLecs([]);
+    setError("");
+    try {
+      const response = await axios.post(
+        `http://localhost:3500/student/getAllLecs`, {
+        rfid: user.rfid,
+        course: user.course
+      });
+      setLecs(response.data.AllLecs);
+    } catch (err) {
+      setError(err);
+    }
+  }
+  useEffect(() => {
+    fetchallLectures();
+  }, []);
+
   console.log(user);
 
   // fetching Attendance
@@ -23,7 +44,7 @@ const AttendanceStudent = () => {
       const response = await axios.post(
         `http://localhost:3500/student/getattendance`, {
         rfid: user.rfid,
-        course:user.course
+        course: user.course
       });
       setAttendance(response.data.attendance);
     } catch (err) {
@@ -92,6 +113,43 @@ const AttendanceStudent = () => {
                 <li>Subject : {record.hardwaredetails.Subject}</li>
                 <li>Teacher : {record.hardwaredetails.Teacher}</li>
                 <li>Attendance : {record.attendance}</li>
+              </ul>
+              <br />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No recent Records To Display</p>
+      )}
+
+      <h2 className="mb-2 mt-3 whitespace-break-spaces text-4xl font-bold text-violet-950 underline decoration-inherit decoration-2 underline-offset-4 dark:mt-0 dark:text-slate-400 md:text-6xl">
+        Total Lecs
+      </h2>
+      <section className="attendance__head">
+        <form className="w-full gap-4 accent-violet-900 md:flex ">
+
+          <div className="flex items-end">
+            <button
+              className="mb-4 h-10 rounded-md border-[1.5px] border-solid border-violet-900 bg-slate-800 px-8 py-2 font-semibold tracking-wide text-slate-200 hover:bg-violet-900 focus:bg-violet-900 disabled:cursor-not-allowed dark:border-violet-300 dark:bg-violet-900 dark:text-violet-100 dark:hover:bg-slate-900 md:w-auto"
+              type="submit"
+              onClick={(e) => fetchallLectures(e)}
+            >
+              Fetch Total Lecs
+            </button>
+          </div>
+        </form>
+      </section>
+      <div>{error ? <ErrorStrip error={error.message} /> : ""}</div>
+
+      {AllLecs.length > 0 ? (
+        <div>
+          {AllLecs.map((record, index) => (
+            <div key={index}>
+              <ul>
+                <li>Classrrom : {record.venue}</li>
+                <li>Lecture Date : {convertToIST12HourFormatWithDate(record.sTime)}</li>
+                <li>Subject : {record.Subject}</li>
+                <li>Teacher : {record.Teacher}</li>
               </ul>
               <br />
             </div>
